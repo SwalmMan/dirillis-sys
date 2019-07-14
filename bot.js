6b74835f1060cf92adb056e86902eb860696ff75
SwalmMan <52794465+SwalmMan@users.noreply.github.com>
2019-07-14 16:02:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['ahelp','Type ahelp','soon-قريبا'];
+    var setGame = ['ahelp','Type ahelp','El Swalm-السوالم'];
     var i = -1;
     var j = 0;
     setInterval(function (){
